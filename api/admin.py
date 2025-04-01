e6d2e309f7aff20d8114ec80f53db427f1a77c68
leloeduk <leloeduk2025@gmail.com>
2025-04-01 16:38:19 +0200

    feat:modify models

--- api/admin.py
+++ api/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from .models import Classe, Matiere, Chapitre, Devoir, Document, BEPEC
+from .models import Classe, Matiere, Chapitre, Devoir, Document, BEPEC, Corrige, CorrigeBEPEC
 
 class ClasseAdmin(admin.ModelAdmin):
     list_display = ('nom', 'author')
     search_fields = ('nom',)
 
 class MatiereAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'classe', 'author')
-    search_fields = ('nom', 'classe__nom')
+    list_display = ('nom', 'author')
+    search_fields = ('nom',)
+
+
+class CorrigeAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'devoir', 'author')
+    search_fields = ('titre', 'devoir__titre', 'author__username')
+    list_filter = ('devoir', 'author')
 
 class ChapitreAdmin(admin.ModelAdmin):
     list_display = ('titre', 'matiere', 'author')
@@ -24,9 +30,19 @@ class DocumentAdmin(admin.ModelAdmin):
     list_filter = ('categorie', 'date')
 
 class BEPECAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'matière', 'classe', 'date')
-    search_fields = ('nom', 'matière__nom', 'classe__nom')
+    list_display = ('nom', 'matiere', 'classe', 'annee_examen', 'date')
+    search_fields = ('nom', 'matiere__nom', 'classe__nom', 'annee_examen')
 
+class CorrigeBEPECAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'bepec', 'author')
+    search_fields = ('titre', 'bepec__nom', 'author__username')
+    list_filter = ('bepec', 'author')
+
+
+
+
+admin.site.register(CorrigeBEPEC, CorrigeBEPECAdmin)
+admin.site.register(Corrige, CorrigeAdmin)
 admin.site.register(Classe, ClasseAdmin)
 admin.site.register(Matiere, MatiereAdmin)
 admin.site.register(Chapitre, ChapitreAdmin)
--- api/models.py
+++ api/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
-from django.contrib.auth.models import User  # Importer le modèle User de Django
+from django.contrib.auth.models import User
 
 class Classe(models.Model):
     nom = models.CharField(max_length=50)
@@ -13,11 +10,11 @@ class Classe(models.Model):
 
 class Matiere(models.Model):
     nom = models.CharField(max_length=50)
-    classe = models.ForeignKey(Classe, related_name='matieres', on_delete=models.CASCADE)
+    classes = models.ManyToManyField(Classe, related_name='matieres')
     author = models.ForeignKey(User, related_name='matieres', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.nom} ({self.classe.nom})'
+        return self.nom
 
 class Chapitre(models.Model):
     titre = models.CharField(max_length=100)
@@ -40,13 +37,44 @@ class Devoir(models.Model):
     matiere = models.ForeignKey(Matiere, related_name='devoirs', on_delete=models.CASCADE)
     fichier_pdf = models.FileField(upload_to='devoirs_pdfs/')
     date = models.DateField()
-    annee = models.CharField(max_length=4)  # Ajout du champ pour l'année
-    classe = models.ForeignKey(Classe, related_name='devoirs', on_delete=models.CASCADE)  # Lien vers la classe
-    categorie = models.CharField(choices=CATEGORIES, max_length=20)  # Ajout du champ pour la catégorie
-    enseignant = models.ForeignKey(User, related_name='devoirs_composes', on_delete=models.CASCADE)  # Ajout du champ pour l'enseignant
+    annee = models.CharField(max_length=4)
+    classe = models.ForeignKey(Classe, related_name='devoirs', on_delete=models.CASCADE)
+    categorie = models.CharField(choices=CATEGORIES, max_length=20)
+    enseignant = models.ForeignKey(User, related_name='devoirs_composes', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.titre} ({self.matiere.nom})'
+
+class Corrige(models.Model):
+    titre = models.CharField(max_length=100)
+    description = models.TextField()
+    fichier_pdf = models.FileField(upload_to='corriges_pdfs/')
+    devoir = models.ForeignKey(Devoir, related_name='corriges', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='corriges', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Corrigé de {self.devoir.titre}'
+
+class BEPEC(models.Model):
+    nom = models.CharField(max_length=200)
+    date = models.DateField()
+    annee_examen = models.CharField(max_length=4)
+    matiere = models.ForeignKey(Matiere, on_delete=models.CASCADE)
+    classe = models.ForeignKey(Classe, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.nom} - {self.matiere.nom} - {self.classe.nom} ({self.annee_examen})"
+
+class CorrigeBEPEC(models.Model):
+    titre = models.CharField(max_length=100)
+    description = models.TextField()
+    fichier_pdf = models.FileField(upload_to='corriges_bepec_pdfs/')
+    bepec = models.ForeignKey(BEPEC, related_name='corriges', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='corriges_bepec', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Corrigé de {self.bepec.nom}'
 
 class Document(models.Model):
     CATEGORIES = [
@@ -55,7 +83,7 @@ class Document(models.Model):
         ('pedagogiques', 'Fichiers pédagogiques'),
         ('autres', 'Autres'),
     ]
-    
+
     titre = models.CharField(max_length=100)
     description = models.TextField()
     fichier_pdf = models.FileField(upload_to='documents_pdfs/')
@@ -67,13 +95,22 @@ class Document(models.Model):
     def __str__(self):
         return f'{self.titre} - {self.categorie} ({self.classe.nom})'
 
-
-class BEPEC(models.Model):
-    nom = models.CharField(max_length=200)  # Nom du BEPEC
-    date = models.DateField()  # Date de l'examen
-    matière = models.ForeignKey(Matiere, on_delete=models.CASCADE)  # Lien vers la matière
-    classe = models.ForeignKey(Classe, on_delete=models.CASCADE)  # Lien vers la classe
-    description = models.TextField(blank=True, null=True)  # Description de l'examen BEPEC
+class Corrige(models.Model):
+    titre = models.CharField(max_length=100)
+    description = models.TextField()
+    fichier_pdf = models.FileField(upload_to='corriges_pdfs/')
+    devoir = models.ForeignKey(Devoir, related_name='corriges', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='corriges', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.nom} - {self.matière.nom} - {self.classe.nom}"
+        return f'Corrigé de {self.devoir.titre}'
+
+class CorrigeBEPEC(models.Model):
+    titre = models.CharField(max_length=100)
+    description = models.TextField()
+    fichier_pdf = models.FileField(upload_to='corriges_bepec_pdfs/')
+    bepec = models.ForeignKey(BEPEC, related_name='corriges', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='corriges_bepec', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Corrigé de {self.bepec.nom}'
