30d35a80c512437bfb7e6d26620983dbd1a6b1af
leloeduk <leloeduk2025@gmail.com>
2025-04-01 20:01:25 +0200

    feat: fix serialisezs

--- api/api/serializers.py
+++ api/api/serializers.py
@@ -1,29 +1,33 @@
 from rest_framework import serializers
 from api.models import Classe, Matiere, Chapitre, Devoir, Document, BEPEC, Corrige, CorrigeBEPEC
 
-class ClasseSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
-
-    class Meta:
-        model = Classe
-        fields = ['id', 'nom', 'author']
-
-class MatiereSerializer(serializers.ModelSerializer):
-    classes = ClasseSerializer(many=True, read_only=True)
-    author = serializers.StringRelatedField(read_only=True)
-
-    class Meta:
-        model = Matiere
-        fields = ['id', 'nom', 'classes', 'author']
-
 class ChapitreSerializer(serializers.ModelSerializer):
-    matiere = serializers.PrimaryKeyRelatedField(queryset=Matiere.objects.all())
+    # Le champ fichier_pdf est conservé tel quel, en respectant la clé du modèle
     author = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Chapitre
-        fields = ['id', 'titre', 'matiere', 'fichier_pdf', 'author']
+        fields = ['id', 'titre', 'fichier_pdf', 'author']
+        
+class MatiereSerializer(serializers.ModelSerializer):
+    # On imbrique les chapitres en utilisant le related_name 'chapitres' défini dans le modèle Chapitre
+    chapitres = ChapitreSerializer(many=True, read_only=True)
+    author = serializers.StringRelatedField(read_only=True)
 
+    class Meta:
+        model = Matiere
+        # Ici, on renvoie id, nom, la liste imbriquée des chapitres et l'auteur
+        fields = ['id', 'nom', 'chapitres', 'author']
+        
+class ClasseSerializer(serializers.ModelSerializer):
+    # On imbrique les matières en utilisant le related_name 'matieres' défini dans le modèle Matiere (ManyToManyField)
+    matieres = MatiereSerializer(many=True, read_only=True)
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Classe
+        fields = ['id', 'nom', 'matieres', 'author']
+        
 class DevoirSerializer(serializers.ModelSerializer):
     matiere = serializers.PrimaryKeyRelatedField(queryset=Matiere.objects.all())
     classe = serializers.PrimaryKeyRelatedField(queryset=Classe.objects.all())
@@ -32,7 +36,7 @@ class DevoirSerializer(serializers.ModelSerializer):
     class Meta:
         model = Devoir
         fields = ['id', 'titre', 'description', 'matiere', 'fichier_pdf', 'date', 'annee', 'classe', 'categorie', 'enseignant']
-
+        
 class CorrigeSerializer(serializers.ModelSerializer):
     devoir = serializers.PrimaryKeyRelatedField(queryset=Devoir.objects.all())
     author = serializers.StringRelatedField(read_only=True)
@@ -40,7 +44,7 @@ class CorrigeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Corrige
         fields = ['id', 'titre', 'description', 'fichier_pdf', 'devoir', 'author']
-
+        
 class BEPECSerializer(serializers.ModelSerializer):
     matiere = serializers.PrimaryKeyRelatedField(queryset=Matiere.objects.all())
     classe = serializers.PrimaryKeyRelatedField(queryset=Classe.objects.all())
@@ -48,7 +52,7 @@ class BEPECSerializer(serializers.ModelSerializer):
     class Meta:
         model = BEPEC
         fields = ['id', 'nom', 'date', 'annee_examen', 'matiere', 'classe', 'description']
-
+        
 class CorrigeBEPECSerializer(serializers.ModelSerializer):
     bepec = serializers.PrimaryKeyRelatedField(queryset=BEPEC.objects.all())
     author = serializers.StringRelatedField(read_only=True)
@@ -56,7 +60,7 @@ class CorrigeBEPECSerializer(serializers.ModelSerializer):
     class Meta:
         model = CorrigeBEPEC
         fields = ['id', 'titre', 'description', 'fichier_pdf', 'bepec', 'author']
-
+        
 class DocumentSerializer(serializers.ModelSerializer):
     classe = serializers.PrimaryKeyRelatedField(queryset=Classe.objects.all())
     author = serializers.StringRelatedField(read_only=True)
